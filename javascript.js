ca6fee1d56c221850d67d27dc45dc8bcc2760d2d
Funto <funto.afuwape@gmail.com>
2022-10-22 23:25:10 +0100

    Create getComputerChoice function
    The getComputerChoice function was created and tested with console.log and confirmed to be functional. It returns either rock, paper, or scissors

---
+++ javascript.js
@@ -0,0 +1,26 @@
+/* 
+1. Create a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+
+2. Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+    Make your function’s playerSelection parameter case-insensitive (so users   can input rock, ROCK, RocK or any other variation).
+
+3. Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+
+ */
+
+// Let's start with 1
+//We can get it to randomly select one of the three options by writing a function that returns a random number between one and three and assigning an option to each number
+//To do this, we'll use the math methods for getting random numbers and then an if else if 
+function getComputerChoice() {
+    let computerChoice = Math.floor((Math.random() * 3) + 1);
+    if (computerChoice === 1) {
+        return "Rock";
+    } else if (computerChoice === 2) {
+        return "Paper";
+    } else if (computerChoice === 3) {
+        return "Scissors"
+    } else {
+        return "Something's gone horribly wrong"
+    }
+}
+
